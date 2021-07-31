0824a0be456672b4e2ec4f4a2edb0b6427d34c7c
poikilos <7557867+poikilos@users.noreply.github.com>
2021-07-31 12:39:00 -0400

    Handle errors and show the exact messages.

--- always_add/home/maria/Projects/BackupNow/BackupNow.sh
+++ always_add/home/maria/Projects/BackupNow/BackupNow.sh
@@ -16,7 +16,9 @@ customExit(){
     msg="$1"
     code="$2"
     if [ -z "$1" ]; then
-        msg="Unknown Error $2"
+        msg="Unknown Error $code"
+    elif [ ! -z "$code" ]; then
+        msg="Error code $code: $msg"
     fi
     xmessage -buttons Ok:0 -default Ok -nearmouse "$msg"
     exit $code
@@ -60,10 +62,20 @@ mkdir -p "$DST_PROFILE/Documents"
 mkdir -p "$DST_PROFILE/Desktop"
 mkdir -p "$DST_PROFILE/Projects"
 echo "* copying Desktop..."
-rsync -tr --info=progress2 "$HOME/Desktop/" "$DST_PROFILE/Desktop"
+src="$HOME/Desktop"
+rsync -tr --info=progress2 "$src/" "$DST_PROFILE/Desktop"
+code=$?
+if [ $code -ne 0 ]; then
+    customExit "Copying $src failed. Look at the black Terminal window behind this message to see the errors and copy and paste them to somewhere." $code
+fi
 #cp ~/Desktop/ $DST_PROFILE/Desktop
 echo "* copying Projects..."
-rsync -tr --info=progress2 "$HOME/Projects/" "$DST_PROFILE/Projects"
+src="$HOME/Projects"
+rsync -tr --info=progress2 "$src/" "$DST_PROFILE/Projects"
+code=$?
+if [ $code -ne 0 ]; then
+    customExit "Copying $src failed. Look at the black console window to see the errors and copy and paste them to somewhere." $code
+fi
 #cp -Rvf ~/Projects/ $DST_PROFILE/Projects
 echo "* copying Firefox Bookmarks..."
 #formerly fdp08zhg:
@@ -72,14 +84,24 @@ this_dest_path="$DST_PROFILE/.mozilla/firefox/8bvxllvz.default/bookmarkbackups"
 mkdir -p "$this_dest_path"
 rsync -tr --info=progress2 "$this_source_path/" "$this_dest_path"
 #^ formerly had no slash
-echo "* copying Documents..."
-rsync -tr --info=progress2 --exclude-from='/home/maria/exclude.txt' "$HOME/Documents/" "$DST_PROFILE/Documents"
-#cp -Rvf "$HOME/Documents/" "$DST_PROFILE/Documents"
-if [ $? -ne 0 ]; then
-    xmessage -buttons Ok:0 -default Ok -nearmouse "Error: backup $DST_PROFILE/Documents failed. Try re-inserting $targetvol."
-  #( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.25s ; kill -9 $pid
-  #( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.25s ; kill -9 $pid
-  #( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.25s ; kill -9 $pid
-    exit 1
+code=$?
+if [ $code -ne 0 ]; then
+    xmessage -buttons Ok:0 -default Ok -nearmouse "Copying $src failed with code $code. Look at the black Terminal window behind this message to see the errors and copy and paste them to somewhere."
 fi
+echo "* copying Documents..."
+src="$HOME/Documents"
+rsync -tr --info=progress2 --exclude-from='/home/maria/exclude.txt' "$src/" "$DST_PROFILE/Documents"
+code=$?
+if [ $code -ne 0 ]; then
+    customExit "Copying $src failed. Look at the black console window to see the errors and copy and paste them to somewhere." $code
+fi
+#cp -Rvf "$HOME/Documents/" "$DST_PROFILE/Documents"
+#if [ $? -ne 0 ]; then
+#    xmessage -buttons Ok:0 -default Ok -nearmouse "Error: backup $DST_PROFILE/Documents failed. Try re-inserting $targetvol."
+  #( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.25s ; kill -9 $pid
+  #( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.25s ; kill -9 $pid
+  #( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.25s ; kill -9 $pid
+    #exit 1
+#fi
 xmessage -buttons Ok:0 -default Ok -nearmouse "The backup completed successfully."
+exit 0
